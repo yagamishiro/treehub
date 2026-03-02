310c30a303cb17183e180abb8dcfcb33933f1342
Gamaliel Lumanta <gamaliel.lumanta@centrix.one>
2026-03-03 03:37:04 +0800

    Added ssl

--- backend/db.ts
+++ backend/db.ts
@@ -12,6 +12,7 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+  ssl: { rejectUnauthorized: true },
 });
 
 export async function initDb() {
