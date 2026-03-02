351d23dd09ca1af2005d564e38bb0cc05d4d0252
Gamaliel Lumanta <gamaliel.lumanta@centrix.one>
2026-03-03 06:08:13 +0800

    Modified cors

--- backend/index.ts
+++ backend/index.ts
@@ -15,8 +15,12 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+const FRONTEND_URL = process.env.FRONTEND_URL
 
-app.use(cors());
+app.use(cors({
+  origin: FRONTEND_URL,
+  credentials: true, // allow cookies/auth headers
+}));
 app.use(express.json());
 app.use(cookieParser());
 
